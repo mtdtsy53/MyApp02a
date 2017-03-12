61a99801d46889893133b0adfd42c4fc7127274a
mtd tsy <mtdtsy53@gmail.com>
2017-03-12 12:13:39 +0900

    commit 3

--- app/src/main/java/com/example/tm/myapp02/MainActivity.java
+++ app/src/main/java/com/example/tm/myapp02/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // comment 3
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // comment 1
